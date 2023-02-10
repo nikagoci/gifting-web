81537043632684fb24ada804a4dc28268943b817
nikagoci <dato.svanidze2005@mail.ru>
2023-02-10 16:28:44 +0400

    add footer+404 page+add react icons to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import Layout from '@/components/layout/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  if(router.pathname === '/404'){
+    return <Component {...pageProps} />
+  }
+  
   return (
     <Layout>
       <Component {...pageProps} />
