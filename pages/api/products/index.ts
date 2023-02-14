b29c0811145f1b0d8be11dd02b09e88d2e73c202
nikagoci <dato.svanidze2005@mail.ru>
2023-02-14 14:24:28 +0400

    add pagination to backend

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -2,14 +2,28 @@ import connectToDatabase from "@/database/connectDB";
 import Product from "@/database/model/productModel";
 import { NextApiRequest, NextApiResponse } from "next";
 
+function pagination(req: NextApiRequest) {
+    let limit: number = 10;
+    let skip: number = 0;
+    if(req.query.page && req.query.limit){
+        const page =  +req.query.page || 1;
+        limit = +req.query.limit;
+        skip = (page - 1) * limit;
+    }
+
+    return {skip, limit}
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method === 'GET'){
         try{
             
             await connectToDatabase();
+
+            const {skip, limit} = pagination(req)
             
-            const products = await Product.find();
-    
+            const products = await Product.find().skip(skip).limit(limit);
+
             res.status(200).json({
                 status: 'success',
                 quantity: products.length,
