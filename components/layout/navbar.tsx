d1240c108c7863745bc644831e442f44a8d3390b
nikagoci <dato.svanidze2005@mail.ru>
2023-02-17 12:07:53 +0400

    change navbar to authencated users with responsive details

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -2,8 +2,9 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import Button from "../shared/ui/button";
+import { useSession } from "next-auth/react";
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
@@ -11,14 +12,28 @@ function classNames(...classes: any) {
 
 export default function Navbar() {
   const router = useRouter();
+  const { data: session, status } = useSession();
+
+  console.log({
+    session,
+    status,
+  });
+
+  if (status === "loading") {
+    return <h1>Loading...</h1>;
+  }
 
   const active = "border-indigo-500 text-gray-900";
   const notActive =
     "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700";
 
+  const activeMobile = "bg-indigo-50 border-indigo-500 text-indigo-700";
+  const notActiveMobile =
+    "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700";
+
   return (
     <Disclosure as="nav" className="bg-white shadow">
-      {({ open }) => (
+      {({ open, close }) => (
         <>
           <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
@@ -28,11 +43,13 @@ export default function Navbar() {
                     className="block w-auto h-8 lg:hidden"
                     src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                     alt="Workflow"
+                    onClick={() => close()}
                   />
                   <img
                     className="hidden w-auto h-8 lg:block"
                     src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
                     alt="Workflow"
+                    onClick={() => close()}
                   />
                 </Link>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
@@ -67,12 +84,83 @@ export default function Navbar() {
                 </div>
               </div>
 
-              <div className="hidden sm:ml-6 sm:flex sm:items-center gap-x-4">
-                <Button href="/signup">Sing Up</Button>
-                <Button full href="/login">
-                  Log In
-                </Button>
-              </div>
+              {status === "unauthenticated" && (
+                <div className="hidden sm:ml-6 sm:flex sm:items-center gap-x-4">
+                  <Button href="/signup">Sing Up</Button>
+                  <Button full href="/login">
+                    Log In
+                  </Button>
+                </div>
+              )}
+
+              {status === "authenticated" && (
+                <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="flex text-sm bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="w-8 h-8 rounded-full"
+                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Your Profile
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Settings
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Sign out
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              )}
 
               <div className="flex items-center -mr-2 sm:hidden">
                 {/* Mobile menu button */}
@@ -89,35 +177,96 @@ export default function Navbar() {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="pt-2 pb-3 space-y-1">
+            <div className="flex flex-col pt-2 pb-3 space-y-1">
               {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
               <Link
+                onClick={() => close()}
                 href="/"
                 className={`
-                    inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2 ${
-                      router.pathname === "/" ? active : notActive
+                    inline-flex items-center px-4 py-3 text-sm font-medium border-b-2 ${
+                      router.pathname === "/" ? activeMobile : notActiveMobile
                     }
                     `}
               >
                 Home
               </Link>
               <Link
+                onClick={() => close()}
                 href="/#about"
                 scroll={false}
-                className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent cursor-pointer hover:border-gray-300 hover:text-gray-700"
+                className="inline-flex items-center px-4 py-3 text-sm font-medium text-gray-500 border-b-2 border-transparent cursor-pointer hover:border-gray-300 hover:text-gray-700"
               >
                 About
               </Link>
               <Link
+                onClick={() => close()}
                 href="/products/?page=1"
                 className={`
-                    inline-flex items-center px-1 pt-1 text-sm font-medium  border-b-2 ${
-                      router.pathname === "/products" ? active : notActive
+                    inline-flex items-center px-4 py-3 text-sm font-medium  border-b-2 ${
+                      router.pathname === "/products"
+                        ? activeMobile
+                        : notActiveMobile
                     }
                     `}
               >
                 Products
               </Link>
+              {status === "unauthenticated" && (
+                <div className="flex px-4 pt-4 gap-x-4">
+                  <Button
+                    padding="py-2 px-4 text-sm"
+                    href="/signup"
+                    onClick={() => close()}
+                  >
+                    Sing Up
+                  </Button>
+                  <Button
+                    padding="py-2 px-6 text-sm"
+                    full
+                    href="/login"
+                    onClick={() => close()}
+                  >
+                    Log In
+                  </Button>
+                </div>
+              )}
+              {status === 'authenticated' && (
+                <div className="pt-4 pb-3 border-t border-gray-200">
+                <div className="flex items-center px-4">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="w-10 h-10 rounded-full"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div className="ml-3">
+                    <div className="text-base font-medium text-gray-800">Tom Cook</div>
+                    <div className="text-sm font-medium text-gray-500">tom@example.com</div>
+                  </div>
+                </div>
+                <div className="mt-3 space-y-1">
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+                  >
+                    Your Profile
+                  </a>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+                  >
+                    Settings
+                  </a>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+                  >
+                    Sign out
+                  </a>
+                </div>
+              </div>
+              )}
             </div>
 
             {/* There Should Be Mob Nav if User Is Register 1) */}
--- components/shared/ui/button.tsx
+++ components/shared/ui/button.tsx
@@ -6,15 +6,17 @@ interface Props {
     full?: boolean;
     padding?: string;
     href: string;
-    type?: string
+    type?: string;
+    onClick?: () => void
 }
 
-export default function Button({ children, full, padding, href, }: Props) {
+export default function Button({ children, full, padding, href, onClick }: Props) {
 
     if (full) {
         return <Link href={href}
             type="button"
             className={`inline-flex items-center  text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${padding ? padding : 'px-8 py-2'}`}
+            onClick={onClick}
         >
             {children}
         </Link>
@@ -24,6 +26,7 @@ export default function Button({ children, full, padding, href, }: Props) {
         <Link href={href}
             type="button"
             className={`inline-flex items-center text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${padding ? padding : 'px-8 py-2'}`}
+            onClick={onClick}
         >
             {children}
         </Link>
