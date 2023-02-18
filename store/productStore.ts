df1b80cebf18927fda1a48d4812edd1c787009f5
nikagoci <dato.svanidze2005@mail.ru>
2023-02-18 14:57:15 +0400

    initliazi easy-peasy for state managment

---
+++ store/productStore.ts
@@ -0,0 +1,60 @@
+import { createStore, action, Action, persist } from "easy-peasy";
+
+interface Products {
+    name: string;
+    image: string;
+    city: string;
+    category: string;
+    gender: string;
+    description: string;
+}
+
+interface ProductModel {
+    products: Products
+}
+
+type Name = string;
+type Image = string;
+type City = string;
+type Category = string;
+type Gender = string;
+type Description = string;
+
+export interface StoreModel {
+    products: Products;
+    addName: Action<ProductModel, Name>;
+    addImage: Action<ProductModel, Image>;
+    addCity: Action<ProductModel, City>;
+    addCategory: Action<ProductModel, Category>;
+    addGender: Action<ProductModel, Gender>;
+    addDescription: Action<ProductModel, Description>;
+}
+
+export const productStore = createStore<StoreModel>({
+  products: persist({
+    name: "",
+    image: "",
+    city: "",
+    category: "",
+    gender: "",
+    description: ""
+  }),
+  addName: action((state, payload) => {
+    state.products.name = payload;
+  }),
+  addImage: action((state, payload) => {
+    state.products.image = payload
+  }),
+  addCity: action((state, payload) => {
+    state.products.city = payload
+  }),
+  addCategory: action((state, payload) => {
+    state.products.category = payload
+  }),
+  addGender: action((state, payload) => {
+    state.products.gender = payload;
+  }),
+  addDescription: action((state, payload) => {
+    state.products.description = payload
+  })
+});
