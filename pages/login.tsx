84d3f19d6eb79f3d320e95dcfc5929afaec927e7
nikagoci <dato.svanidze2005@mail.ru>
2023-02-18 21:24:16 +0400

    remove all files

--- pages/login.tsx
+++
@@ -1,7 +0,0 @@
-import LoginUser from "@/components/forms/login-user";
-
-export default function LoginPage() {
-    return(
-        <LoginUser />
-    )
-}
--- pages/signup.tsx
+++
@@ -1,7 +0,0 @@
-import SignpUser from "@/components/forms/signup-user";
-
-export default function SignupPage() {
-    return(
-        <SignpUser />
-    )
-}
