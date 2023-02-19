a0fbeb014dccdf2998ea20dc7d4224572187c9ad
nikagoci <dato.svanidze2005@mail.ru>
2023-02-19 12:48:50 +0400

    created modal after user submit post(users can now see product they added)

--- components/post/submit-post.tsx
+++ components/post/submit-post.tsx
@@ -1,4 +1,10 @@
-import { FormEvent, useEffect, useState } from "react";
+import {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import CheckboxList from "./checkbox-list";
 import { ToastContainer } from "react-toastify";
 import toastError from "@/utils/toastErrors";
@@ -6,10 +12,15 @@ import toastError from "@/utils/toastErrors";
 import "react-toastify/dist/ReactToastify.css";
 import { useStoreActions, useStoreState } from "@/store/hooks";
 import { Products } from "@/store/ProductStore";
-import toastSuccess from "@/utils/toastSuccess";
 import { useRouter } from "next/router";
+import Spinner from "../shared/ui/spinner";
+import Modal from "../shared/modal";
 
-const createProduct = async (productState: Products) => {
+const createProduct = async (
+  productState: Products,
+  setIsLoading: Dispatch<SetStateAction<boolean>>
+) => {
+  setIsLoading(true);
   const response = await fetch("/api/product", {
     method: "POST",
     body: JSON.stringify({
@@ -22,28 +33,33 @@ const createProduct = async (productState: Products) => {
   });
 
   const data = await response.json();
+  setIsLoading(false);
 
   return data;
 };
 
 const clearSessionStorage = () => {
   sessionStorage.clear();
-  
-}
+};
 
 export default function SubmitPost() {
   const productStore = useStoreState((state) => state.products);
-  const clearProducts = useStoreActions((state) => state.clearProducts)
+  const clearProducts = useStoreActions((state) => state.clearProducts);
   const [checked, setChecked] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [productId, setProductId] = useState('');
   const router = useRouter();
 
   useEffect(() => {
-    
-    if(!productStore.description || !productStore.gender || !productStore.image || !productStore.name) {
-      router.push('/post/add-product')
+    if (
+      !productStore.description ||
+      !productStore.gender ||
+      !productStore.image ||
+      !productStore.name
+    ) {
+      router.push("/post/add-product");
     }
-
-  }, [])
+  }, []);
 
   async function submitHandler(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -53,24 +69,39 @@ export default function SubmitPost() {
         "You can not submit post without accept usage of phone number"
       );
     } else {
-      const response = await createProduct(productStore);
+      const response = await createProduct(productStore, setIsLoading);
 
       if (response.status === "success") {
-        clearSessionStorage()
-        clearProducts()
-        toastSuccess("Successfully submitted");
-        setTimeout(() => {
-          router.push("/");
-        }, 1500);
-        
-      } else{
-        toastError('Something went wrong. try again later')
+        clearSessionStorage();
+        clearProducts();
+        setProductId(response.product._id)
+      } else {
+        toastError("Something went wrong. try again later");
       }
     }
   }
 
+
+  if (isLoading) {
+    return (
+      <div className="mt-20">
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          draggable
+          theme="light"
+        />
+        <Spinner size={40} />
+      </div>
+    );
+  }
+
   return (
     <>
+    {productId && <Modal id={productId} />}
       <ToastContainer
         position="top-center"
         autoClose={3000}
---
+++ components/shared/modal.tsx
@@ -0,0 +1,94 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+
+export default function Modal({id}: {id: string}) {
+  const [open, setOpen] = useState(true);
+
+  const cancelButtonRef = useRef(null);
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+              <div>
+                <div className="flex items-center justify-center w-12 h-12 mx-auto bg-green-100 rounded-full">
+                  <CheckIcon
+                    className="w-6 h-6 text-green-600"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="text-center mt-7 sm:mt-5">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Product successfully added
+                  </Dialog.Title>
+                  <div className="mt-6">
+                    <p className="text-sm text-gray-500">
+                      Do you want to see your product or go back to home page?
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+                <Link
+                  href={`/products/${id}`}
+                  className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                  onClick={() => setOpen(false)}
+                >
+                  See Product
+                </Link>
+                <Link
+                  href="/"
+                  className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+                  onClick={() => setOpen(false)}
+                  ref={cancelButtonRef}
+                >
+                  Go back to home
+                </Link>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
