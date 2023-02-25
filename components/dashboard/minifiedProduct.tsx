52d4c151c3bf1fd335f3fb6274a4702940c71d96
nikagoci <dato.svanidze2005@mail.ru>
2023-02-25 18:03:36 +0400

    fetch products from database by user

--- components/dashboard/minifiedProduct.tsx
+++ components/dashboard/minifiedProduct.tsx
@@ -1,54 +1,51 @@
 import React from 'react'
 
 import {
-    ArrowNarrowLeftIcon,
-    ArrowNarrowRightIcon,
-    BriefcaseIcon,
-    CalendarIcon,
     CheckCircleIcon,
-    CheckIcon,
-    ChevronDownIcon,
     ChevronRightIcon,
-    CurrencyDollarIcon,
-    LinkIcon,
-    LocationMarkerIcon,
-    MailIcon,
-    PencilIcon,
-    SearchIcon,
   } from '@heroicons/react/solid'
+import { ProductInterface } from '@/utils/interfaces'
+import Image from 'next/image'
+import Link from 'next/link'
 
-const MinifiedProduct = ({candidate}: {candidate: any}) => {
+const MinifiedProduct = ({product}: {product: ProductInterface}) => {
+  
+  function myLoader() {
+    return product.imageSrc
+  }
+
+  const capitalizedCity = product.city.charAt(0).toUpperCase()+ product.city.slice(1, product.city.length)
+
   return (
-    <li key={candidate.email}>
-    <a href="#" className="block group">
+    <li>
+    <Link href={`products/${product._id}`} className="block group">
       <div className="flex items-center px-4 py-5 sm:py-6 sm:px-0">
         <div className="flex items-center flex-1 min-w-0">
           <div className="flex-shrink-0">
-            <img
-              className="w-12 h-12 rounded-full group-hover:opacity-75"
-              src={candidate.imageUrl}
-              alt=""
+            <Image
+            loader={myLoader}
+              className="rounded-full group-hover:opacity-75"
+              src={product.imageSrc}
+              alt={product.name}
+              width={24}
+              height={24}
             />
           </div>
           <div className="flex-1 min-w-0 px-4 md:grid md:grid-cols-2 md:gap-4">
             <div>
-              <p className="text-sm font-medium text-purple-600 truncate">{candidate.name}</p>
+              <p className="text-sm font-medium text-purple-600 truncate">{product.name}</p>
               <p className="flex items-center mt-2 text-sm text-gray-500">
-                <MailIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                <span className="truncate">{candidate.email}</span>
+                <span className="truncate">{capitalizedCity}</span>
               </p>
             </div>
             <div className="hidden md:block">
               <div>
-                <p className="text-sm text-gray-900">
-                  Applied on <time dateTime={candidate.appliedDatetime}>{candidate.applied}</time>
-                </p>
                 <p className="flex items-center mt-2 text-sm text-gray-500">
                   <CheckCircleIcon
                     className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
                     aria-hidden="true"
                   />
-                  {candidate.status}
+                  Currently Active
                 </p>
               </div>
             </div>
@@ -61,7 +58,7 @@ const MinifiedProduct = ({candidate}: {candidate: any}) => {
           />
         </div>
       </div>
-    </a>
+    </Link>
   </li>
   )
 }
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,7 +4,8 @@ import MinifiedProduct from "@/components/dashboard/minifiedProduct";
 import { getSession } from "next-auth/react";
 import Image from "next/image";
 import User from "@/database/model/userModel";
-import { UserInterface } from "@/utils/interfaces";
+import { ProductInterface, UserInterface } from "@/utils/interfaces";
+import Product from "@/database/model/productModel";
 
 const candidates = [
   {
@@ -28,104 +29,112 @@ const candidates = [
 ];
 
 interface Props {
-    user: UserInterface
+  user: UserInterface;
+  products: ProductInterface[];
 }
 
-export default function DashboardPage({user}: Props) {
-  const userName = user.email.split('@')[0];
+export default function DashboardPage({ user, products }: Props) {
+  const userName = user.email.split("@")[0];
 
+  console.log(products);
   return (
-        <div className="relative min-h-screen bg-white">
-          <div className="max-w-3xl px-4 mx-auto sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
-            <div className="flex items-center py-8 space-x-5">
-              <div className="flex-shrink-0">
-                <div className="relative">
-                <Image
-                          className="rounded-full"
-                          src="/user.png"
-                          alt="user"
-                          width={42}
-                          height={42}
-                        />
-                  <span
-                    className="absolute inset-0 rounded-full shadow-inner"
-                    aria-hidden="true"
-                  />
-                </div>
-              </div>
-              <div className="flex flex-col gap-y-1">
-                <h1 className="text-2xl font-bold text-gray-900">
-                  {userName}
-                </h1>
-                <p className="text-sm font-medium text-gray-500">
-                  Email-
-                  <span className="text-gray-900">
-                    {user.email}
-                  </span>{" "}
-                </p>
-                <p className="text-sm font-medium text-gray-500">
-                  Phone Number-
-                  <span className="text-gray-900">
-                    {user.phoneNumber}
-                  </span>{" "}
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col-reverse mt-6 space-y-4 space-y-reverse justify-stretch sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3">
-              <button
-                type="button"
-                className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
-              >
-                Remove Account
-              </button>
-              <button
-                type="button"
-                className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
-              >
-                Add Product
-              </button>
+    <div className="relative min-h-screen bg-white">
+      <div className="max-w-3xl px-4 mx-auto sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
+        <div className="flex items-center py-8 space-x-5">
+          <div className="flex-shrink-0">
+            <div className="relative">
+              <Image
+                className="rounded-full"
+                src="/user.png"
+                alt="user"
+                width={42}
+                height={42}
+              />
+              <span
+                className="absolute inset-0 rounded-full shadow-inner"
+                aria-hidden="true"
+              />
             </div>
           </div>
-          <main className="pt-8 pb-16">
-            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-              {/* Stacked list */}
-              <ul
-                role="list"
-                className="mt-5 border-t border-gray-200 divide-y divide-gray-200 sm:mt-0 sm:border-t-0"
-              >
-                {candidates.map((candidate) => (
-                  <MinifiedProduct key={candidate.name} candidate={candidate} />
-                ))}
-              </ul>
-            </div>
-          </main>
+          <div className="flex flex-col gap-y-1">
+            <h1 className="text-2xl font-bold text-gray-900">{userName}</h1>
+            <p className="text-sm font-medium text-gray-500">
+              Email-
+              <span className="text-gray-900">{user.email}</span>{" "}
+            </p>
+            <p className="text-sm font-medium text-gray-500">
+              Phone Number-
+              <span className="text-gray-900">{user.phoneNumber}</span>{" "}
+            </p>
+          </div>
         </div>
+        <div className="flex flex-col-reverse mt-6 space-y-4 space-y-reverse justify-stretch sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3">
+          <button
+            type="button"
+            className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+          >
+            Remove Account
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+          >
+            Add Product
+          </button>
+        </div>
+      </div>
+      <main className="pt-8 pb-16">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          {/* Stacked list */}
+          <ul
+            role="list"
+            className="mt-5 border-t border-gray-200 divide-y divide-gray-200 sm:mt-0 sm:border-t-0"
+          >
+            {products.map((product) => (
+              <MinifiedProduct key={product._id} product={product} />
+            ))}
+          </ul>
+        </div>
+      </main>
+    </div>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async(context) => {
-    const session = await getSession({req: context.req})
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
 
-  
-    if(!session){
-      return {
-        redirect: {
-          destination: '/signup',
-          permanent: false
-        }
-      }
-    }
-
-    const user = await User.findOne({email: session.user?.email})
-  
-    if(context.locale){
-      return {
-        props: {
-          user: JSON.parse(JSON.stringify(user)),
-          ...( await serverSideTranslations(context.locale, ['addproduct', 'common']))
-        }
-      }
-    }
-  
-    throw new Error('Locale not found')
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signup",
+        permanent: false,
+      },
+    };
   }
+
+  let user;
+  let products;
+  try {
+    user = await User.findOne({ email: session.user?.email });
+    const productIds = JSON.parse(JSON.stringify(user.products));
+
+    products = await Product.find({
+      _id: { $in: productIds },
+    }).lean();
+  } catch (err: any) {}
+
+  if (context.locale) {
+    return {
+      props: {
+        user: JSON.parse(JSON.stringify(user)),
+        products: JSON.parse(JSON.stringify(products)),
+        ...(await serverSideTranslations(context.locale, [
+          "addproduct",
+          "common",
+        ])),
+      },
+    };
+  }
+
+  throw new Error("Locale not found");
+};
