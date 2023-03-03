76f8a30f3113f9a89b309622dec27c3f6cb15ab2
nikagoci <dato.svanidze2005@mail.ru>
2023-03-03 19:58:00 +0400

    using svg for flags/close switcher when clicking outside

--- components/shared/langSwitcher.tsx
+++ components/shared/langSwitcher.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
+
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 export default function LangSwitcher() {
   const [active, setActive] = useState("en");
   const [showMenu, setShowMenu] = useState(false);
   const router = useRouter();
+  const langSwitcherRef = useRef<HTMLDivElement>(null);
 
   const usa = (
     <Image src="/usa.svg" width={24} height={24} alt="United States" />
@@ -22,35 +25,52 @@ export default function LangSwitcher() {
     router.push(router.asPath, undefined, { locale });
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (langSwitcherRef.current && !langSwitcherRef.current.contains(event.target as Node)) {
+        setShowMenu(false);
+      }
+    };
+
+    window.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [langSwitcherRef]);
+
   return (
-    <div className="relative z-30 p-2 rounded bg-slate-50">
-      <div
-        className="flex items-center justify-center space-x-1 cursor-pointer"
-        onClick={() => setShowMenu((prev) => !prev)}
-      >
-        {active === "en" ? usa : georgia}
-        {!showMenu ? <IoIosArrowDown size={20} /> : <IoIosArrowUp size={20} />}
-      </div>
-      {showMenu && (
-        <div
-          className={`${
-            active === "ka" ? "flex-col" : "flex-col-reverse"
-          } absolute flex items-center justify-center w-full py-8 -translate-x-1/2 border rounded gap-y-4 bg-slate-50 -bottom-32 left-1/2`}
-        >
-          {router.locales &&
-            router.locales.map((locale) => (
-              <Image
-                key={locale}
-                src={`${locale === "en" ? "/usa.svg" : "/georgia.png"}`}
-                alt="United States"
-                className="cursor-pointer"
-                width={40}
-                height={40}
-                onClick={() => handleChangeLang(locale)}
-              />
-            ))}
+    <>
+        <div ref={langSwitcherRef} className="relative z-30 p-2 rounded bg-slate-50">
+          <div
+            className="flex items-center justify-center space-x-1 cursor-pointer"
+            onClick={() => setShowMenu((prev) => !prev)}
+          >
+            {active === "en" ? <span className="fi fi-us"></span> : <span className="fi fi-ge"></span>}
+            {!showMenu ? (
+              <IoIosArrowDown size={20} />
+            ) : (
+              <IoIosArrowUp size={20} />
+            )}
+          </div>
+          {showMenu && (
+            <div
+              className={`${
+                active === "ka" ? "flex-col" : "flex-col-reverse"
+              } absolute flex items-center justify-center w-full py-4 -translate-x-1/2 border rounded gap-y-4 bg-slate-50 -bottom-[105px] left-1/2`}
+            >
+              {router.locales &&
+                router.locales.map((locale) => (
+                  <span
+                    key={locale}
+                    className={`fi ${locale === 'en' ? 'fi-us' : 'fi-ge'} cursor-pointer w-full h-7 `}
+                    onClick={() => handleChangeLang(locale)}
+                  >
+                    </span>
+                ))}
+            </div>
+          )}
         </div>
-      )}
-    </div>
+    </>
   );
 }
